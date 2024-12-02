1b95f2d86bc1dfd5b6157985a20e902566aacd89
geovannarc <geovannarcf@gmail.com>
2024-12-02 04:47:28 -0300

    adiciona rota de buscar usuarios

--- src/main/java/org/example/dto/UserDataDTO.java
+++ src/main/java/org/example/dto/UserDataDTO.java
@@ -1,12 +1,14 @@
 package org.example.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Getter
 public class UserDataDTO {
 
     private String username;
